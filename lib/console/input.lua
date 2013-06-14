859b2432167c7ab789fc42c73ba9011eea243120
Minh Ngo <mkngo87@gmail.com>
2013-06-14 18:50:39 -0400

    input: major update; Can now store command history and new/removed functions

--- lib/console/input.lua
+++ lib/console/input.lua
@@ -1,48 +1,112 @@
-local enter = {
-	kpenter   = true,
-	['return']= true,
-}
-
-local endKeypress = function(self)
-	self.remaining_delay    = self.repeat_delay
-	self.remaining_interval = self.repeat_interval
-	self.last_keypressed    = nil
-	self.last_unicode       = nil
-end
-
 local input = class 'Input'
 
-function input:init(repeat_delay,repeat_interval)
+function input:init(repeat_delay,repeat_interval,max_history)
 	local t = self
-	t.chars             = {}
-	t.cursor_pos        = 1
-	t.repeat_delay      = repeat_delay or 0.2
-	t.repeat_interval   = repeat_interval or 0.05
-	t.last_keypressed   = nil
-	t.last_unicode      = nil
-	t.remaining_delay   = t.repeat_delay
-	t.remaining_interval= t.repeat_interval
+	t.chars              = {}
+	t.history            = {}
+	t.max_history        = max_history or 20
+	t.cursor_pos         = 1
+	t.repeat_delay       = repeat_delay or 0.2
+	t.repeat_interval    = repeat_interval or 0.05
+	t.previous_index     = 0
+	
+	-- PRIVATE
+	t._last_keypressed   = nil
+	t._last_unicode      = nil
+	t._remaining_delay   = t.repeat_delay
+	t._remaining_interval= t.repeat_interval
 end
+
+-- #######################################
+-- HOOKS
+-- #######################################
+
+local key_hooks = {
+	kpenter   = function(self) input.flush(self) end,
+	
+	['return']= function(self) input.flush(self) end,
+	
+	backspace = function(self)
+		self.cursor_pos = self.cursor_pos-1
+		if not self.chars[self.cursor_pos] then self.cursor_pos = 1 return end
+		table.remove(self.chars,self.cursor_pos)
+	end,
+	
+	delete = function(self)
+		table.remove(self.chars,self.cursor_pos)
+	end,
+	
+	home = function(self)
+		input.setCursorPos(self,1)
+	end,
+	
+	['end'] = function(self)
+		input.setCursorPos(self,-1)
+	end,
+	
+	left = function(self)
+		input.setCursorPos(self, input.getCursorPos(self)-1 )
+	end,
+	
+	right = function(self)
+		input.setCursorPos(self, input.getCursorPos(self)+1 )
+	end,
+	
+	up = function(self)
+		input.setPrevious(self, self.previous_index+1)
+	end,
+	
+	down = function(self)
+		local prev_index = self.previous_index
+		if not input.setPrevious(self, prev_index-1) then
+			input.clear(self)
+			self.previous_index = 0
+		end
+	end,
+}
+
+-- #######################################
+-- OVERRIDEABLE CALLBACKS
+-- #######################################
 
 function input:onFlush(str)
 	
 end
+
+-- #######################################
+-- USEFUL FUNCTIONS
+-- #######################################
 
 function input:clear()
 	self.chars      = {}
 	self.cursor_pos = 1
 end
 
-function input:iterate()
-	return ipairs(self.chars)
+function input:flush()
+	local str = table.concat(self.chars)
+	if str ~= '' and str ~= self.history[1] then
+		table.insert(self.history,1,str)
+		local history_size = #self.history
+		while history_size > self.max_history do
+			table.remove(self.history) 
+		end
+	end
+	if self.onFlush then self:onFlush(str) end
+	input.clear(self)
 end
 
-function input:getString()
-	return table.concat(self.chars)
-end
-
-function input:getLength()
-	return #self.chars
+function input:setPrevious(index)
+	local command    = self.history[index]
+	if not command then return false end
+	self.chars       = {}
+	local cursor_pos = 1
+	for char in command:gmatch '.' do
+		cursor_pos = cursor_pos + 1
+		table.insert(self.chars,char)
+	end
+	self.cursor_pos     = cursor_pos
+	self.previous_index = index
+	return true
 end
 
 function input:getChar(char_pos)
@@ -55,53 +119,43 @@ function input:setCursorPos(cursor_pos)
 	self.cursor_pos = math.min(#self.chars, math.max(cursor_pos,1))
 end
 
-function input:getCursorPos()
-	return self.cursor_pos
-end
+function input:getCursorPos() return self.cursor_pos end
+
+-- #######################################
+-- LOVE CALLBACKS
+-- #######################################
 
 function input:keypressed(key,unicode)
-	if enter[key] then
-		local str = table.concat(self.chars)
-		self:onFlush(str)
-		input.clear(self)
-	elseif key == 'backspace' then
-		self.cursor_pos = self.cursor_pos-1
-		if not self.chars[self.cursor_pos] then self.cursor_pos = 1 return end
-		table.remove(self.chars,self.cursor_pos)
-	elseif key == 'delete' then
-		table.remove(self.chars,self.cursor_pos)
-	elseif key == 'home' then
-		input.setCursorPos(self,1)
-	elseif key == 'end' then
-		input.setCursorPos(self,-1)
+	if key_hooks[key] then 
+		key_hooks[key](self)
 	elseif unicode > 31 and unicode < 127 then
 		table.insert(self.chars,self.cursor_pos,string.char(unicode))
 		self.cursor_pos = self.cursor_pos + 1
 	end
 	
-	if self.last_keypressed ~= key then
-		self.remaining_delay    = self.repeat_delay
-		self.remaining_interval = self.repeat_interval
+	if self._last_keypressed ~= key then
+		self._remaining_delay    = self.repeat_delay
+		self._remaining_interval = self.repeat_interval
 	end
 	
-	self.last_keypressed= key
-	self.last_unicode   = unicode
-end
-
-function input:keyreleased(key)
-	if key == self.last_keypressed then
-		endKeypress(self)
-	end
+	self._last_keypressed= key
+	self._last_unicode   = unicode
 end
 
 function input:update(dt)
-	if not self.last_keypressed then return end
-	self.remaining_delay = self.remaining_delay - dt
-	if self.remaining_delay <= 0 then
-		self.remaining_interval = self.remaining_interval - dt
-		if self.remaining_interval <= 0 then
-			self.remaining_interval = self.repeat_interval
-			input.keypressed(self,self.last_keypressed,self.last_unicode)
+	if not (self._last_keypressed and 
+		love.keyboard.isDown(self._last_keypressed) )then 
+		
+		self._remaining_delay   = self.repeat_delay
+		self._remaining_interval= self.repeat_interval
+		return 
+	end
+	self._remaining_delay = self._remaining_delay - dt
+	if self._remaining_delay <= 0 then
+		self._remaining_interval = self._remaining_interval - dt
+		if self._remaining_interval <= 0 then
+			self._remaining_interval = self.repeat_interval
+			input.keypressed(self,self._last_keypressed,self._last_unicode)
 		end
 	end
 end
