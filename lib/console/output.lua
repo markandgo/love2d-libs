030935f3fbe456c2c00c4e5fa29debfc7e9f85f2
Minh Ngo <mkngo87@gmail.com>
2013-06-15 04:11:41 -0400

    output: Added output:resize and removed unneccessary funcs

--- lib/console/output.lua
+++ lib/console/output.lua
@@ -1,32 +1,48 @@
 local output = class 'Output'
 
-function output:init(chars_width,max_size)
+function output:init(chars_width,max_lines)
 	self.lines       = {}
-	self.max_size    = max_size or 100
-	self.buffer_size = 0
+	self.max_lines   = max_lines or 100
 	self.chars_width = chars_width or math.huge
+end
+
+function output:resize(chars_width)
+	chars_width = chars_width or self.chars_width
+	if chars_width == self.chars_width then return end
+	self.lines      = {}
+	local str_cache = {}
+	for index,line in ipairs(self.lines) do
+		local nextline = self.lines[index+1]
+		if nextline and nextline.wrapped or line.wrapped then
+			table.insert(str_cache,line)
+		else
+			if next(str_cache) then
+				local str = table.concat(str_cache)
+				str_cache = {} 
+				output.write(self,str)
+			end
+			output.write(self,line)
+		end
+	end
 end
 
 function output:write(str)
 	for line in str:lines() do
+		local count = 1
 		repeat
 			local newline = line:sub(1,self.chars_width)
+			if count == 2 then newline = {str = newline,wrapped = true} end
 			table.insert(self.lines,newline)
-			line = line:sub(self.chars_width+1)
+			line  = line:sub(self.chars_width+1)
+			count = count + 1
 		until line == ''
 	end
 	
-	local len = #str
-	local new_buffer_size = self.buffer_size+len
-	while new_buffer_size > self.max_size do
-		new_buffer_size = new_buffer_size - #table.remove(self.lines,1)
+	local lines = #self.lines
+	while lines > self.max_lines do
+		lines = lines - 1
+		table.remove(self.lines,1)
 	end
-	self.buffer_size = new_buffer_size
-end
-
-function output:getLine(row)
-	if row < 0 then row = #self.lines+1+row end
-	return self.lines[row]
 end
 
 function output:iterate(reverseOrder)
@@ -38,30 +54,6 @@ function output:iterate(reverseOrder)
 		index = index + delta
 		return line
 	end
-end
-
-function output:getLineCount()
-	return #self.lines
-end
-
-function output:getBufferSize()
-	return self.buffer_size
-end
-
-function output:getMaxSize()
-	return self.max_size
-end
-
-function output:setMaxSize(max_size)
-	self.max_size = max_size or 100
-end
-
-function output:setWidth(chars_width)
-	self.chars_width = chars_width
-end
-
-function output:getWidth()
-	return self.chars_width
 end
 
 return output
